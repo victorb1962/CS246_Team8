fa016e817a5a972a942be14539c1c9563055c9e2
VictorB <78133439+victorb1962@users.noreply.github.com>
2021-03-30 04:05:16 -0500

    Add files via direct upload

---
+++ app/src/main/java/com/example/cs246_team8/ViewGradeList/ViewGradeListAdapter.java
@@ -0,0 +1,132 @@
+package com.example.cs246_team8.ViewGradeList;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.cs246_team8.R;
+import com.example.cs246_team8.utils;
+
+import java.util.ArrayList;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * The type View grade list adapter.
+ */
+// Adapted from https://developer.android.com/guide/topics/ui/layout/recyclerview
+public class ViewGradeListAdapter extends RecyclerView.Adapter<ViewGradeListAdapter.GradeViewHolder> {
+    private RecyclerItemClickListener listener;
+    private ArrayList<ViewGradeListItem> mDataset;
+
+    /**
+     * The type Grade view holder.
+     */
+// Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class GradeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        // each data item is just a string in this case
+
+        /**
+         * The Layout.
+         */
+        public RelativeLayout layout;
+        private RecyclerItemClickListener listener;
+
+        /**
+         * Instantiates a new Grade view holder.
+         * @param v the v
+         * @param l the l
+         */
+        public GradeViewHolder(RelativeLayout v, RecyclerItemClickListener l) {
+            super(v);
+            layout = v;
+            listener = l;
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onClick(view, getAdapterPosition());
+        }
+    }
+
+    /**
+     * Instantiates a new View grade list adapter.
+     * @param l the l
+     */
+// Provide a suitable constructor (depends on the kind of dataset)
+    public ViewGradeListAdapter(RecyclerItemClickListener l) {
+        mDataset = new ArrayList<ViewGradeListItem>();
+        listener = l;
+    }
+
+    /**
+     * Update.
+     * @param newdata the newdata
+     */
+    public void update(ArrayList<ViewGradeListItem> newdata) {
+        mDataset.clear();
+        mDataset.addAll(newdata);
+
+        notifyDataSetChanged();
+    }
+
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public GradeViewHolder onCreateViewHolder(ViewGroup parent,
+                                                               int viewType) {
+        // create a new view
+        RelativeLayout v = (RelativeLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.gradeview_item, parent, false);
+
+        GradeViewHolder vh = new GradeViewHolder(v, listener);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(GradeViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+
+        TextView item_name = holder.layout.findViewById(R.id.gradeviewname);
+        TextView item_grade = holder.layout.findViewById(R.id.gradeviewnum);
+
+        ViewGradeListItem item = mDataset.get(position);
+
+        if (position % 2 == 0){
+            holder.layout.setBackgroundColor(Color.LTGRAY);
+        } else {
+            holder.layout.setBackgroundColor(Color.WHITE);
+
+        }
+
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) item_name.getLayoutParams();
+        if (item.is_category){
+            item_name.setTypeface(null, Typeface.BOLD);
+            params.leftMargin = utils.dp_to_pixels(20);
+        } else {
+            item_name.setTypeface(null, Typeface.NORMAL);
+            params.leftMargin = utils.dp_to_pixels(40);
+        }
+
+        item_name.setText(item.name);
+
+        String gradepercentage = item.grade == null ? "- %" : String.format("%.2f%%", item.grade) ;
+
+        item_grade.setText(gradepercentage);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
---
+++ app/src/main/java/com/example/cs246_team8/ViewGradeList/ViewGradeListItem.java
@@ -0,0 +1,60 @@
+package com.example.cs246_team8.ViewGradeList;
+
+/**
+ * View grade list item instantiates a new View grade list item.
+ */
+public class ViewGradeListItem {
+    /**
+     * The Is category.
+     */
+    public boolean is_category;
+    /**
+     * The Name.
+     */
+    public String name;
+    /**
+     * The Category id.
+     */
+    public int category_id;
+    /**
+     * The Assignment id.
+     */
+    public int assignment_id;
+    /**
+     * The Grade.
+     */
+    public Float grade;
+
+    /**
+     * Instantiates a new View grade list item.
+     * @param is_category   the is category
+     * @param name          the name
+     * @param category_id   the category id
+     * @param assignment_id the assignment id
+     * @param grade         the grade
+     */
+    public ViewGradeListItem(boolean is_category, String name, int category_id, int assignment_id, Float grade){
+        this.is_category = is_category;
+        this.name = name;
+        this.category_id = category_id;
+        this.assignment_id = assignment_id;
+        this.grade = grade;
+    }
+
+    /**
+     * Instantiates a new View grade list item.
+     * @param is_category   the is category
+     * @param name          the name
+     * @param category_id   the category id
+     * @param assignment_id the assignment id
+     */
+    public ViewGradeListItem(boolean is_category, String name, int category_id, int assignment_id){
+        this(is_category, name, category_id, assignment_id, null);
+    }
+
+    public void set_grade(Float grade){
+        this.grade = grade;
+    }
+    public float get_grade_total(String name){ return this.grade;}
+
+}
